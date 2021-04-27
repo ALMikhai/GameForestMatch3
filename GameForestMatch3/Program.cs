543491fbe22d59dd93eff97b5b0e22cc10a83d6c
AlexanderMikhailenko <molotok539@gmail.com>
2021-04-27 19:21:28 +1000

    Init MonoGame project

---
+++ GameForestMatch3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameForestMatch3
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game())
+                game.Run();
+        }
+    }
+}
