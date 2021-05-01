f3c1e0d55432215a1686be78a4c07be4a7a467ba
AlexanderMikhailenko <molotok539@gmail.com>
2021-05-01 15:57:58 +1000

    Add class Scene. Add GameScene. Add score label

---
+++ GameForestMatch3/Scenes/GameScene.cs
@@ -0,0 +1,40 @@
+﻿using GameForestMatch3.Tiles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameForestMatch3.Scenes
+{
+    public class GameScene : Scene
+    {
+        private BoardView _boardView;
+        private int _score;
+        private Vector2 _scorePosition = new Vector2(25, 25);
+
+        public GameScene(int boardRows, int boardColumns, SpriteFont font) : base(font)
+        {
+            _boardView = new BoardView(new BoardModel(boardRows, boardColumns), new Vector2(440, 25));
+            _boardView.BoardModel.TileDeleted += OnTileDeleted;
+        }
+
+        private void OnTileDeleted(Tile tile, TilePosition position)
+        {
+            _score += tile.Prize;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _boardView.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            _boardView.Draw(spriteBatch);
+            spriteBatch.DrawString(Font, $"Score: {_score}", _scorePosition, Color.White);
+        }
+
+        public override void MouseClick(Vector2 position)
+        {
+            _boardView.MouseClick(position);
+        }
+    }
+}
---
+++ GameForestMatch3/Scenes/Scene.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameForestMatch3.Scenes
+{
+    public abstract class Scene
+    {
+        public SpriteFont Font { get; }
+
+        public Scene(SpriteFont font)
+        {
+            Font = font;
+        }
+        
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(SpriteBatch spriteBatch);
+        public abstract void MouseClick(Vector2 position);
+    }
+}
