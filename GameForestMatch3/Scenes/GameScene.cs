36def9e2778ad9f6b79a18ead63b456a25cf2e8b
AlexanderMikhailenko <molotok539@gmail.com>
2021-05-01 16:22:27 +1000

    Add Timer. Add Timer on GameScene

--- GameForestMatch3/Scenes/GameScene.cs
+++ GameForestMatch3/Scenes/GameScene.cs
@@ -6,14 +6,29 @@ namespace GameForestMatch3.Scenes
 {
     public class GameScene : Scene
     {
+        public delegate void GameEventHandler();
+
+        public event GameEventHandler GameOver;
+
         private BoardView _boardView;
         private int _score;
         private Vector2 _scorePosition = new Vector2(25, 25);
+        private Vector2 _timerPosition = new Vector2(25, 100);
+        private Vector2 _boardPosition = new Vector2(440, 25);
+        private readonly Timer _timer;
 
-        public GameScene(int boardRows, int boardColumns, SpriteFont font) : base(font)
+        public GameScene(int boardRows, int boardColumns, int gameTimeMilliseconds, SpriteFont font) : base(font)
         {
-            _boardView = new BoardView(new BoardModel(boardRows, boardColumns), new Vector2(440, 25));
+            _boardView = new BoardView(new BoardModel(boardRows, boardColumns), _boardPosition);
             _boardView.BoardModel.TileDeleted += OnTileDeleted;
+
+            _timer = new Timer(gameTimeMilliseconds);
+            _timer.TimeOut += OnTimeOut;
+        }
+
+        private void OnTimeOut()
+        {
+            GameOver?.Invoke();
         }
 
         private void OnTileDeleted(Tile tile, TilePosition position)
@@ -24,12 +39,14 @@ namespace GameForestMatch3.Scenes
         public override void Update(GameTime gameTime)
         {
             _boardView.Update(gameTime);
+            _timer.Update(gameTime);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             _boardView.Draw(spriteBatch);
             spriteBatch.DrawString(Font, $"Score: {_score}", _scorePosition, Color.White);
+            spriteBatch.DrawString(Font, $"Timer: {_timer.SecondsLeft}", _timerPosition, Color.White);
         }
 
         public override void MouseClick(Vector2 position)
---
+++ GameForestMatch3/Timer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GameForestMatch3
+{
+    public class Timer
+    {
+        public delegate void TimerEventHandler();
+
+        public event TimerEventHandler TimeOut;
+
+        public int SecondsLeft => _milliseconds / 1000;
+
+        private int _milliseconds;
+
+        public Timer(int milliseconds)
+        {
+            _milliseconds = milliseconds;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (_milliseconds <= 0)
+            {
+                TimeOut?.Invoke();
+            }
+            else
+            {
+                _milliseconds -= gameTime.ElapsedGameTime.Milliseconds;
+            }
+        }
+    }
+}
