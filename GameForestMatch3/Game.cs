e6896ea47ff68c1384f4e65768e4c34264971f69
AlexanderMikhailenko <molotok539@gmail.com>
2021-05-01 20:07:26 +1000

    Add scaling for windows of different resolutions. Add scenes switch logic

--- GameForestMatch3/Game.cs
+++ GameForestMatch3/Game.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using GameForestMatch3.Button;
+using GameForestMatch3.Scenes;
+using GameForestMatch3.Tiles;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,28 +9,75 @@ namespace GameForestMatch3
 {
     public class Game : Microsoft.Xna.Framework.Game
     {
-        private GraphicsDeviceManager _graphics;
+        public static readonly float StandardWidth = 1920;
+        public static readonly float StandardHeight = 1080;
+        private readonly Matrix _inputScaleMatrix;
+        private readonly Matrix _screenScaleMatrix;
+
+        private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private MouseState _previousMouseState;
+        private SpriteFont _generalFont;
+
+        private MenuScene _menuScene;
+        private GameOverScene _gameOverScene;
+        private GameScene _gameScene;
+        private Scene _currentScene;
 
         public Game()
         {
             _graphics = new GraphicsDeviceManager(this);
+
+            var heightScreenScale = _graphics.PreferredBackBufferHeight / StandardHeight;
+            var widthScreenScale = _graphics.PreferredBackBufferWidth / StandardWidth;
+            var heightInputScale = StandardHeight / _graphics.PreferredBackBufferHeight;
+            var widthInputScale = StandardWidth / _graphics.PreferredBackBufferWidth;
+            _screenScaleMatrix = Matrix.CreateScale(widthScreenScale, heightScreenScale, 1.0f);
+            _inputScaleMatrix = Matrix.CreateScale(widthInputScale, heightInputScale, 1.0f);
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
+            _previousMouseState = Mouse.GetState();
         }
 
         protected override void LoadContent()
         {
+            TileFactory.ContentManager = Content;
+            ButtonFactory.ContentManager = Content;
+            
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _generalFont = Content.Load<SpriteFont>("GeneralFont");
 
-            // TODO: use this.Content to load your game content here
+            _menuScene = new MenuScene(_generalFont);
+            _menuScene.PlayButton.Click += OnPlayButtonClick;
+
+            _gameOverScene = new GameOverScene(_generalFont);
+            _gameOverScene.OkButton.Click += OnOkButtonClick;
+
+            _currentScene = _menuScene;
+        }
+
+        private void OnOkButtonClick()
+        {
+            _currentScene = _menuScene;
+        }
+
+        private void OnPlayButtonClick()
+        {
+            _gameScene = new GameScene(8, 8, 60000, _generalFont);
+            _gameScene.GameOver += OnGameOver;
+            _currentScene = _gameScene;
+        }
+
+        private void OnGameOver()
+        {
+            _gameScene.GameOver -= OnGameOver;
+            _currentScene = _gameOverScene;
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,7 +86,16 @@ namespace GameForestMatch3
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (_previousMouseState.LeftButton == ButtonState.Pressed
+                && Mouse.GetState().LeftButton == ButtonState.Released)
+            {
+                var correctPosition = Vector2.Transform(Mouse.GetState().Position.ToVector2(), _inputScaleMatrix);
+                _currentScene.MouseClick(correctPosition);
+            }
+
+            _previousMouseState = Mouse.GetState();
+
+            _currentScene.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -44,9 +103,11 @@ namespace GameForestMatch3
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, _screenScaleMatrix);
 
-            // TODO: Add your drawing code here
+            _currentScene.Draw(_spriteBatch);
 
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
